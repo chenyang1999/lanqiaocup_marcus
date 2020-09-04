e70c48c2126065d39b2fa4697d6b2cdf4c368196
ChenYang <374263410@qq.com>
2020-09-04 14:21:39 +0800

    upDATE

---
+++ .leetcode/13.罗马数���转整数.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=13 lang=python3
+#
+# [13] 罗马数字转整数
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        Roman2Int = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        Int = 0
+        n = len(s)
+
+        for index in range(n - 1):
+            if Roman2Int[s[index]] < Roman2Int[s[index + 1]]:
+                Int -= Roman2Int[s[index]]
+            else:
+                Int += Roman2Int[s[index]]
+
+        return Int + Roman2Int[s[-1]]
+# @lc code=end
+
---
+++ .leetcode/18.四数之和.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=18 lang=python3
+#
+# [18] 四数之和
+#
+
+# @lc code=start
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+# @lc code=end
+
